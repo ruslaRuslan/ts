97d6f2230ae6f2d6a6f9a00c17d9c26edea9dd23
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-04-30 15:33:30 +0400

    enum

--- index.ts
+++ index.ts
@@ -35,16 +35,49 @@
 
 // -------------------------------------------------------------------------------------------------------------------------------------
 
-interface User{
+// interface User{
+//     ad: string;
+//     age: 12 | 13 | 15;
+//     job?: "fe"| "be"| "fs"; // optional
+// }
+
+// const ali: User ={
+//     ad: "ali",
+//     age: 13
+// }
+
+// -------------------------------------------------------------------------------------------------------------------------------------
+
+// 0 islemir
+// 1 isleyir
+// 2 cixib
+
+// DNS ->
+enum workStatusEnum {
+    Islemir = 0,
+    Isleyir = 1,
+    Cixib = 2
+}
+
+interface CompanyWorker {
     ad: string;
-    age: 12 | 13 | 15;
-    job?: "fe"| "be"| "fs"; // optional
+    workStatus: workStatusEnum;
+}
+const ali: CompanyWorker = {
+    ad: 'ali',
+    workStatus: workStatusEnum.Isleyir
+}
+enum Controllers {
+    UP = 39,
+    RIGHT = 40,
+    DOWN = 38,
+    LEFT = 37
 }
 
-const ali: User ={
-    ad: "ali",
-    age: 13
+
+
+enum Actions {
+    "GET_USERS",
+    "ADD_USER"
 }
-
-
-
+// dispatch({type:Actions.GET_USERS})
