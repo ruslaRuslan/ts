efbff31208f995abd3e9c0e94de729b5f678f45d
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-04-30 11:41:35 +0400

    global typeScript yukledik

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 // console.log('salam')
 
-let ad = 'ali'
-ad = 12
-console.log(ad.toUpperCase());
+// let ad = "ali";
+// ad = 12;
+// console.log(ad.toUpperCase());
+
+// function cem(a, b) {
+//   return a + b;
+// }
+// console.log(cem(12, 23));
+
